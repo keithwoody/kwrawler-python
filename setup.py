85c03da9222f777f5180d35d3c8015acbc93f4ea
Keith Woody <keith.woody@gmail.com>
2014-01-24 17:26:20 -0800

    flesh out setup, add description to README

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Kwrawler',
+    version='0.1dev',
+    packages=['kwrawler',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
