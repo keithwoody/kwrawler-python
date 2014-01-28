7b97c5ff1790e5aa5467ef0711ee12024be404c1
Keith Woody <keith.woody@gmail.com>
2014-01-28 15:48:04 -0800

    implement and test parsing pages assets
    * add current_contents member
    * don’t init uri
    * add site_dict to store site structure
    * move validate_uri call to traverse_site
    * make from_uri call traverse_site
    * add mock_html and mock_response to traverse_site tests

--- src/kwrawler/sitemap.py
+++ src/kwrawler/sitemap.py
@@ -1,28 +1,85 @@
 # a class encapsulating the site traversal and sitemap rendering logic for the kwrawler module
 import httplib
 from urlparse import urlparse
+import urllib
+import time
+from bs4 import BeautifulSoup
 
 class Sitemap(object):
     URI_FAILURE = "failed to read URI '%s'"
+    current_contents = None
 
     def __init__(self):
-        self.uri = "URI must be set"
+        self.site_dict = { 'pages': [],
+                           'links': {},
+                           'assets': { 'imgs': [],
+                                       'scripts': [],
+                                       'stylesheets': [] }
+                         }
 
-    def from_uri(self, uri):
+    def from_uri(self, uri, render_opts={}):
         """ sets the instance URI and renders a sitemap image
         """
         self.uri = uri
-        if self.validate_uri():
-            # traverse_site( uri )
+        if self.traverse_site( uri ):
+            # img_format = render_opts['format'] or 'png'
+            # render_sitemap( img_format, render_options )
             pass
         else:
             return self.URI_FAILURE % uri
 
-    def validate_uri(self):
+    def traverse_site(self, uri_str):
+        if self.validate_uri( uri_str ):
+          # populate site_dict
+          print "%d traversing %s" % (int(time.time()), uri_str)
+          page_dict = { 'assets': { 'imgs': [],
+                                    'scripts': [],
+                                    'stylesheets': []
+                                  },
+                        'links': []
+                      }
+          self.current_contents = urllib.urlopen( uri_str ).read()
+          soup = BeautifulSoup( self.current_contents )
+          # get all scripts, stylesheets and images
+          for script in soup.find_all('script'):
+              src = script.get('src')
+              if src is not None:
+                  page_dict['assets']['scripts'].append( src )
+          sset = set(self.site_dict['assets']['scripts'])
+          pset = set(page_dict['assets']['scripts'])
+          # list of unique js sources for the site
+          self.site_dict['assets']['scripts'] = list( sset | pset )
+
+          for css in soup.select('link[rel="stylesheet"]'):
+              href = css.get('href')
+              if href is not None:
+                  page_dict['assets']['stylesheets'].append( href )
+          sset = set(self.site_dict['assets']['stylesheets'])
+          pset = set(page_dict['assets']['stylesheets'])
+          # list of unique stylesheets for the site
+          self.site_dict['assets']['stylesheets'] = list( sset | pset )
+
+          for img in soup.select('img'):
+              src = img.get('src')
+              if src is not None and src not in page_dict['assets']['imgs']:
+                  page_dict['assets']['imgs'].append( src )
+          sset = set(self.site_dict['assets']['imgs'])
+          pset = set(page_dict['assets']['imgs'])
+          # list of unique images for the site
+          self.site_dict['assets']['imgs'] = list( sset | pset )
+
+          # get all internal links on the page
+          # add the current page and set it as processed
+          # traverse any linked pages
+          return True
+        else:
+          return False
+
+    def validate_uri(self, uri):
         """ validates a URI is well formed and readable
         """
-        if self.uri is not None:
-          u = urlparse( self.uri )
+        if uri is not None:
+          u = urlparse( uri )
           conn = httplib.HTTPConnection( u.netloc )
           path = u.path or "/"
           try:
@@ -33,7 +90,7 @@ class Sitemap(object):
               else:
                   return False
           except Exception, e:
-              print self.URI_FAILURE % self.uri
+              print self.URI_FAILURE % uri
               return False
           finally:
               conn.close()
--- src/kwrawler/tests/test_sitemap.py
+++ src/kwrawler/tests/test_sitemap.py
@@ -1,10 +1,29 @@
 from kwrawler import sitemap
 import unittest
 import os.path
+from mock import patch, Mock
+from io import StringIO
 
 class SitemapTestCase(unittest.TestCase):
     def setUp(self):
         self.sitemap_obj = sitemap.Sitemap()
+        self.mock_html = """
+        <html>
+          <head>
+            <script src="local.js"></script>
+            <link rel="stylesheet" href="local.css"/>
+          </head>
+          <body>
+            <a href="/internal">internal</a>
+            <a href="/internal#with_anchor">internal</a>
+            <a href="http://external.com/about">external</a>
+            <img src="local.png"/>
+            <hr/>
+            <img src="local.png"/>
+          </body>
+        </html>
+        """
+        self.mock_response = StringIO( unicode(self.mock_html) )
 
     def test_from_uri_should_require_a_URI(self):
         with self.assertRaises( TypeError ):
@@ -17,10 +36,8 @@ class SitemapTestCase(unittest.TestCase):
         self.assertEqual( error_msg, self.sitemap_obj.from_uri('http://not.valid') )
 
     def test_validate_uri(self):
-        self.sitemap_obj.uri = "http://example.com/"
-        self.assertEqual(True, self.sitemap_obj.validate_uri())
-        self.sitemap_obj.uri = "http://not.valid/"
-        self.assertEqual(False, self.sitemap_obj.validate_uri())
+        self.assertEqual(True, self.sitemap_obj.validate_uri("http://example.com/"))
+        self.assertEqual(False, self.sitemap_obj.validate_uri("http://not.valid/"))
 
     def test_render_sitemap_1(self):
         # assert False
@@ -30,9 +47,35 @@ class SitemapTestCase(unittest.TestCase):
         # assert False
         pass
 
-    def test_traverse_site(self):
-        # assert False
-        pass
+    def test_traverse_site_should_set_current_contents(self):
+        assert None == self.sitemap_obj.current_contents
+        self.sitemap_obj.traverse_site( 'http://example.com/' )
+        assert None != self.sitemap_obj.current_contents
+
+    def test_traverse_site_should_add_unique_js_sources_to_assets(self):
+        with patch('urllib.urlopen') as mock_get:
+            mock_get.return_value = self.mock_response
+            self.sitemap_obj.traverse_site( 'http://example.com/' )
+            js_lst = self.sitemap_obj.site_dict['assets']['scripts']
+            self.assertEqual( True, 'local.js' in js_lst )
+            self.assertEqual( 1, len(js_lst) )
+
+    def test_traverse_site_should_add_unique_stylesheets_sources_to_assets(self):
+        with patch('urllib.urlopen') as mock_get:
+            mock_get.return_value = self.mock_response
+            self.sitemap_obj.traverse_site( 'http://example.com/' )
+            css_lst = self.sitemap_obj.site_dict['assets']['stylesheets']
+            self.assertEqual( True, 'local.css' in css_lst )
+            self.assertEqual( 1, len(css_lst) )
+
+    def test_traverse_site_should_add_unique_images_sources_to_assets(self):
+        with patch('urllib.urlopen') as mock_get:
+            mock_get.return_value = self.mock_response
+            self.sitemap_obj.traverse_site( 'http://example.com/' )
+            img_lst = self.sitemap_obj.site_dict['assets']['imgs']
+            self.assertEqual( True, 'local.png' in img_lst )
+            self.assertEqual( 1, len(img_lst) )
+
 
     def test_build_site_graph(self):
         # assert False
